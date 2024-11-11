36ef0979ef9436a188a750a0542225adb1383352
Hari Hara Kumar <34203665+hari-hk@users.noreply.github.com>
2024-11-11 13:03:54 +0000

    change title

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Github actions
         </a>
       </header>
     </div>
